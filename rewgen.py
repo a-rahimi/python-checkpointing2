00bd994cc277aafd297ad697afd3728d3544f496
Ali Rahimi <alrhim@amazon.com>
2020-05-03 20:01:11 -0700

    yield tells process where it resumes

--- rewgen.py
+++ rewgen.py
@@ -10,20 +10,27 @@ def processing():
     lst.append(step)
     print(step, "a=", a, "lst=", lst)
 
-    yield  # Save a checkpoint here
+    t = yield  # Save a checkpoint here
+    if t:
+        print('Resuming from step2')
     step = "step2"
     b = 2
     a *= b
     lst.append(step)
     print(step, "a=", a, "lst=", lst)
 
-    yield  # Save a checkpoint here
+    t = yield  # Save a checkpoint here
+    if t:
+        print('Resuming from step3')
     step = "step3"
     c = 2
     a *= c
     lst.append(step)
     print(step, "a=", a, "lst=", lst)
 
+    t = yield  # Save a checkpoint here
+    if t:
+        print('Resuming from end')
     yield  # Save a checkpoint here
     print("end")
 
@@ -33,10 +40,11 @@ def main():
     gen = processing()
     checkpoints = [copy.deepcopy(jump.save_generator_state(gen)) for _ in gen]
 
-    print("-----Restart processing from the second checkpoint-----")
-    gen_restored = processing()
-    jump.restore_generator(gen_restored, checkpoints[1])
-    for _ in gen_restored:
+    print("-----Restart processing from the first checkpoint-----")
+    gen = processing()
+    jump.restore_generator(gen, checkpoints[0])
+    gen.send(True)
+    for _ in gen:
         pass
 
 
